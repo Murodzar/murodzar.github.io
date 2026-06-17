f447fe2caf155078a055db6531c4ce41c9f6a075
Murodzar <zargar06@gmail.com>
2026-06-17 17:55:43 +0500

    Add files via upload

---
+++ stl-sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'stl-viewer-v1';
+const ASSETS = [
+  '/stl-viewer.html',
+  '/stl-manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
